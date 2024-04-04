d23f6040f3a128fb77f501380efa2f4fda6a250a
karimelkh <karimelkhanoufi22@gmail.com>
2024-04-04 02:07:22 +0000

    update script
    nothing update actually - just adding some comments

--- scripts/init.sh
+++ scripts/init.sh
@@ -6,3 +6,9 @@ b_dir="build"
 if [ ! -d "$b_dir" ]; then
 	mkdir "$b_dir"
 fi
+
+# ensure that `belformat` was installed correctly
+# export LD_LIBRARY_PATH='/usr/local/lib:$LD_LIBRARY_PATH'
+
+# success!!
+echo "project initialized successfully"
